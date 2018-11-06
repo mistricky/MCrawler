8e4b4147fa40258dafccff9ef1fd60f12b1c5e41
HaoDaWang <1306036576@qq.com>
2018-11-06 18:07:13 +0800

    Remove node_modules and build

--- build/module/data-model.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var db_1 = require("./db");
-var data_schema_1 = require("./data-schema");
-exports.DataModel = db_1.db.model("dataModel", data_schema_1.schema, "question-answer");
--- build/module/data-schema.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var mongoose = require("mongoose");
-exports.schema = new mongoose.Schema({
-    question: { type: String },
-    answer: { type: Array }
-});
